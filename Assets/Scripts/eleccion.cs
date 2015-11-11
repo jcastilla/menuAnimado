9fb1ba3acd0a03c9a30761dd957d5bfc47843aab
jcastilla <killofsfsfs@hotmail.com>
2015-11-11 23:26:22 +0100

    Menu animado en Unity
    Proyecto de DSH y .apk

---
+++ Assets/Scripts/eleccion.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eleccion : MonoBehaviour {
+
+	public GameObject miCube;
+	public GameObject global;
+	public float cont = 0.0f;
+	public bool pulsado = false;
+
+	public void empezarAnimacion()
+	{
+		pulsado = true;
+		global.GetComponent<Animator> ().enabled = false;
+		miCube.GetComponent<Animator> ().enabled = true;
+
+
+	}
+
+	void Update()
+	{
+		if (pulsado == true)
+			cont++;
+
+		if (cont > 120) 
+		{
+			pulsado = false;
+			cont = 0;
+			Application.LoadLevel("escena2");
+		}
+	}
+}
